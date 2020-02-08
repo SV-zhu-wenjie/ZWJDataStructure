cfdc84badd8a028983017170fa7320500eeea29e
wenjie.zhu <wenjie.zhu@svr.us>
2020-02-08 16:17:35 +0800

    SmartPointer

---
+++ ZWJLib/Smartpointer.h
@@ -0,0 +1,61 @@
+#ifndef SMARTPOINTER_H
+#define SMARTPOINTER_H
+
+namespace ZWJ
+{
+template <typename T>
+class SmartPoint
+{
+ protected:
+    T* smartPoint;
+public:
+    explicit SmartPoint(T* p = nullptr)
+    {
+        smartPoint = p;
+    }
+
+    SmartPoint(const SmartPoint<T> &obj)
+    {
+        smartPoint = obj.smartPoint;
+        const_cast<SmartPoint<T>&>(obj).smartPoint = nullptr;
+    }
+
+    SmartPoint<T>& operator= (const SmartPoint<T> &obj)
+    {
+        if (this != &obj) {
+            delete smartPoint;
+            smartPoint = obj.smartPoint;
+            const_cast<SmartPoint<T>&>(obj).smartPoint = nullptr;
+        }
+        return *this;
+    }
+
+    T* operator->()
+    {
+        return smartPoint;
+    }
+
+    T& operator* ()
+    {
+        return *smartPoint;
+    }
+
+    bool isNull () const
+    {
+        return smartPoint == nullptr;
+    }
+
+    T* get()
+    {
+        return smartPoint;
+    }
+
+    ~SmartPoint()
+    {
+        delete smartPoint;
+    }
+};
+}
+
+
+#endif // SMARTPOINTER_H
